51f55a616a1e5dd86b33f61a33b32c1ffccb94d5
Makkasugaipova <makkas@hiof.no>
2024-10-07 16:17:38 +0200

    oppgave 2 ferdig

--- src/no/hvl/dat100ptc/oppgave2/GPSData.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSData.java
@@ -8,41 +8,56 @@ public class GPSData {
 	private GPSPoint[] gpspoints;
 	protected int antall = 0;
 
-	public GPSData(int antall) {
-
-		throw new UnsupportedOperationException(TODO.method());
+	// her opprettes en tabell fra konstruktør av objekt GPSPoint
+	public GPSData(int n) {
+		gpspoints = new GPSPoint[n]; //oppretter array
+		antall = 0; //GPS punkter starter med null
 		
-		// TODO
+		/*
+		  Må initialisere en array av objekter slik at vi har et sted å lagre GPSPoint objekter 
+		  når vi leser inn data.
+		 */
 	}
 
+	//returnerer array
 	public GPSPoint[] getGPSPoints() {
 		return this.gpspoints;
 	}
 	
+	/*
+	 Denne metoden setter inn et nytt GPS-punkt i arrayet
+	 og oppdaterer antallet elementer.
+	 */
 	protected boolean insertGPS(GPSPoint gpspoint) {
-
-		boolean inserted = false;
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO 
-	
+	    if (antall < gpspoints.length) { // Sjekker plass i array
+	        gpspoints[antall] = gpspoint; // Setter inn GPS-punkt på rett posisjon
+	        antall++;
+	        return true; 
+	    } else {
+	        return false;
+	    }
+	    /*
+	     Vi må sjekke om det er plass i arrayet før vi setter inn et nytt element 
+	     for å unngå feil. Returnerer true hvis innsettingen var vellykket
+	     og false hvis det ikke er mer plass.
+	     */
 	}
 
 	public boolean insert(String time, String latitude, String longitude, String elevation) {
-
-		GPSPoint gpspoint;
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
-		
+	    
+		GPSPoint gpspoint = GPSDataConverter.convert(time, latitude, longitude, elevation); 
+	    
+		return insertGPS(gpspoint); 
 	}
 
+
 	public void print() {
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
+		
+		
+		System.out.println("====== GPS Data - START ======");
+		for (int i = 0; i < antall; i++) {
+			System.out.println((i + 1) + " " + gpspoints[i].toString());
+		}
+		System.out.println("====== GPS Data - SLUTT ======");
 	}
 }
--- src/no/hvl/dat100ptc/oppgave2/Main.java
+++ src/no/hvl/dat100ptc/oppgave2/Main.java
@@ -3,11 +3,19 @@ package no.hvl.dat100ptc.oppgave2;
 import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class Main {
+    public static void main(String[] args) {
+        // Oppretter objekter
+        GPSPoint gpsPoint1 = new GPSPoint(0, 60.385390, 5.332200, 50.0); 
+        GPSPoint gpsPoint2 = new GPSPoint(1, 60.390000, 5.340000, 70.0); 
 
-	
-	public static void main(String[] args) {
+        // Oppretter et GPSData-objekt med plass til to GPSPoint-objekt
+        GPSData gpsData = new GPSData(2);
 
-		// TODO
-		
-	}
+        // insert metoden
+        gpsData.insertGPS(gpsPoint1);
+        gpsData.insertGPS(gpsPoint2);
+
+        gpsData.print();
+    }
 }
+
